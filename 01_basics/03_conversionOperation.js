2d85287e64e3d24a97c4a091042f93397a063d43
Dhananjay hegde <165047386+dhananjayhegde05@users.noreply.github.com>
2026-02-17 06:49:50 +0000

    container config

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+//type conversion string to number
+// "33" converted => 33
+//"33abc" => NaN = not a number
+//True => 1 False => 0
+
+let isLoggedIn = 1
+let valueBoolean = Boolean(isLoggedIn)
+console.log(valueBoolean)
+
+let no = 33
+let numToString = String(no)
+console.log(numToString)

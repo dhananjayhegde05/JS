bc772b3dc1f7a4d447d2d05959df8d145918a22d
Dhananjay hegde <165047386+dhananjayhegde05@users.noreply.github.com>
2026-02-17 06:29:40 +0000

    container cnfig

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+"use strict";//treat all js coe as newer version
+const accountId = 144553
+let accountEmail = "Jay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bengaluru"
+//accountId = 2
+// console.log(accountId)
+console.log([accountId,accountEmail,accountPassword,accountCity])
+/* prefered not to use var for creating variable  
+because of isue in block scope and functional scope*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"//treates all js code as newer version
+// alert(3 + 3) // alert is not defined bc we are using nodejs , not a browser 
+
+let name = "jay"
+let age = 23
+let isLoggedIn = false
+
+// number 2 to power 53
+//bigint
+//string = ""
+//boolean
+//null => standalone value
+//undefined => without value
+//symbol =>unique
+
+//object
+
+console.log(typeof null) // type of null is object
+console.log(typeof undefined) // type of undefined is undefined
